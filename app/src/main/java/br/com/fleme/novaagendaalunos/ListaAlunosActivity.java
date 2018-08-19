a00779ca7b18b1a4d82e88095b752682c47860f6
Felipe Leme <lemefe21@gmail.com>
2018-08-19 19:45:57 -0300

    Alterando o layout da tela para utilizar o ListView e populando uma lista de String na activity com um adapter

--- app/src/main/java/br/com/fleme/novaagendaalunos/ListaAlunosActivity.java
+++ app/src/main/java/br/com/fleme/novaagendaalunos/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.fleme.novaagendaalunos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Felipe", "Gustavo", "Jeferson", "Ronaldo"};
+        ListView listaAlunosView = findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunosView.setAdapter(adapter);
+
     }
 }
