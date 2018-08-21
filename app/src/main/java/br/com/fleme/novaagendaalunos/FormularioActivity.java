c5043fc982fd1194c62baba2a09a32d3772c599e
Felipe Augusto de Souza Leme <fleme@e-deploy.local>
2018-08-21 11:37:18 -0300

    Colocando o botão de salvar na action bar do formulário e incluindo os arquivos dentro dos recursos.

--- app/src/main/java/br/com/fleme/novaagendaalunos/FormularioActivity.java
+++ app/src/main/java/br/com/fleme/novaagendaalunos/FormularioActivity.java
@@ -3,6 +3,9 @@ package br.com.fleme.novaagendaalunos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,14 +19,24 @@ public class FormularioActivity extends AppCompatActivity {
 
         Log.i("LOG_AGENDA", "onCreate - FormularioActivity");
 
-        Button botaoSalvar = findViewById(R.id.formulario_salvar);
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
                 Toast.makeText(FormularioActivity.this, "Botão Salvar clicado!", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 }
