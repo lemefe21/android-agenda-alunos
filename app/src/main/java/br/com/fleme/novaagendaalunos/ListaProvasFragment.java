7c4f7774d19ad64b715b0205a2d8d79485404376
Felipe Augusto de Souza Leme <fleme@e-deploy.local>
2018-09-11 15:59:32 -0300

    Alterado a construção do nosso layout da tela de provas para iniciarmos a criação das telas com Fragments.

---
+++ app/src/main/java/br/com/fleme/novaagendaalunos/ListaProvasFragment.java
@@ -0,0 +1,63 @@
+package br.com.fleme.novaagendaalunos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.fleme.novaagendaalunos.model.Prova;
+
+public class ListaProvasFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //monta o nosso fragment
+
+        View view = inflater.inflate(R.layout.fragment_lista_provas, container, false);
+
+        List<String> topicosJava = Arrays.asList("Orientação a Objetos", "Classes e Objetos", "Herança e Polimorfismo");
+        Prova provaJava = new Prova("Java", "15/10/2018", topicosJava);
+
+        List<String> topicosAndroid = Arrays.asList("Activitys", "Recursos do Dispositivo", "Layouts");
+        Prova provaAndroid = new Prova("Android", "22/10/2018", topicosAndroid);
+
+        List<String> topicosIonic = Arrays.asList("Instalação e Configuração", "Componentes e Plugins", "Material Design");
+        Prova provaIonic = new Prova("Ionic", "24/10/2018", topicosIonic);
+
+        List<Prova> provas = Arrays.asList(provaJava, provaAndroid, provaIonic);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, provas);
+
+        ListView listView = view.findViewById(R.id.provas_lista);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Prova prova = (Prova) parent.getItemAtPosition(position);
+                Toast.makeText(getContext(), "Clicou na prova " + prova, Toast.LENGTH_SHORT).show();
+
+                Intent intentDetalhesProva = new Intent(getContext(), DetalhesProvaActivity.class);
+                intentDetalhesProva.putExtra("prova", prova);
+
+                startActivity(intentDetalhesProva);
+
+            }
+        });
+
+        return view;
+    }
+}
--- app/src/main/java/br/com/fleme/novaagendaalunos/ProvasActivity.java
+++ app/src/main/java/br/com/fleme/novaagendaalunos/ProvasActivity.java
@@ -1,18 +1,9 @@
 package br.com.fleme.novaagendaalunos;
 
-import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.Arrays;
-import java.util.List;
-
-import br.com.fleme.novaagendaalunos.model.Prova;
 
 public class ProvasActivity extends AppCompatActivity {
 
@@ -21,35 +12,11 @@ public class ProvasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provas);
 
-        List<String> topicosJava = Arrays.asList("Orientação a Objetos", "Classes e Objetos", "Herança e Polimorfismo");
-        Prova provaJava = new Prova("Java", "15/10/2018", topicosJava);
-
-        List<String> topicosAndroid = Arrays.asList("Activitys", "Recursos do Dispositivo", "Layouts");
-        Prova provaAndroid = new Prova("Android", "22/10/2018", topicosAndroid);
-
-        List<String> topicosIonic = Arrays.asList("Instalação e Configuração", "Componentes e Plugins", "Material Design");
-        Prova provaIonic = new Prova("Ionic", "24/10/2018", topicosIonic);
-
-        List<Prova> provas = Arrays.asList(provaJava, provaAndroid, provaIonic);
-
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, provas);
-
-        ListView listView = findViewById(R.id.provas_lista);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Prova prova = (Prova) parent.getItemAtPosition(position);
-                Toast.makeText(ProvasActivity.this, "Clicou na prova " + prova, Toast.LENGTH_SHORT).show();
-
-                Intent intentDetalhesProva = new Intent(ProvasActivity.this, DetalhesProvaActivity.class);
-                intentDetalhesProva.putExtra("prova", prova);
-
-                startActivity(intentDetalhesProva);
-
-            }
-        });
+        //coloca o fragment pronto dentro da tela que está utilizando o FrameLayout
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction tx = fragmentManager.beginTransaction();
+        tx.replace(R.id.frame_principal, new ListaProvasFragment());
+        tx.commit();
 
     }
 }
